a9985113a14e1a8f5dca6a3dc25f7a3fd5f09cce
Vedant <vedantc252@gmail.com>
2025-03-15 20:23:25 +0530

    Adding Frontend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -19,38 +19,42 @@ const prompts_1 = require("@langchain/core/prompts");
 const prompt_1 = require("./prompt");
 const node_1 = require("./defaults/node");
 const react_1 = require("./defaults/react");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 function escapeBraces(str) {
     return str.replace(/{/g, '{{').replace(/}/g, '}}');
 }
-const mistralModel = new mistralai_1.ChatMistralAI({
-    model: "mistral-large-latest",
-    temperature: 0.1,
-    maxTokens: 8000,
-});
 app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const userPrompt = req.body.prompt;
+    const model = new mistralai_1.ChatMistralAI({
+        model: "mistral-large-latest",
+        temperature: 0.1,
+        maxTokens: 8000,
+    });
     const checkProjectType = prompts_1.ChatPromptTemplate.fromMessages([
         ["system", "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra"],
         ["human", userPrompt],
     ]);
-    const chain = checkProjectType.pipe(mistralModel);
+    const chain = checkProjectType.pipe(model);
     const response = yield chain.invoke({});
     const answer = response.content;
     if (answer == "react") {
         res.json({
             // This prompt is for llm so more context
-            prompts: [prompt_1.BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            basePrompt: prompt_1.BASE_PROMPT,
+            filePrompt: `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
             // This one is for frontend so we can make all the files and struture and and everything, thats why passing same thing twice
-            uiPrompts: [react_1.basePrompt]
+            uiPrompts: react_1.basePrompt
         });
         return;
     }
     if (answer === "node") {
         res.json({
-            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
-            uiPrompts: [node_1.basePrompt]
+            basePrompt: prompt_1.BASE_PROMPT,
+            filePrompt: `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            uiPrompts: node_1.basePrompt
         });
         return;
     }
@@ -58,17 +62,35 @@ app.post("/template", (req, res) => __awaiter(void 0, void 0, void 0, function* 
     return;
 }));
 app.post("/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const basePrompt = escapeBraces(req.body.basePrompt);
-    const filePrompt = escapeBraces(req.body.filePrompt);
-    const userPrompt = escapeBraces(req.body.userPrompt);
+    var _a, _b, _c;
+    const basePrompt = escapeBraces((_a = req.body.basePrompt) !== null && _a !== void 0 ? _a : "");
+    const filePrompt = escapeBraces((_b = req.body.filePrompt) !== null && _b !== void 0 ? _b : "");
+    const userPrompt = escapeBraces((_c = req.body.userPrompt) !== null && _c !== void 0 ? _c : "");
     const systemPrompt = escapeBraces((0, prompt_1.getSystemPrompt)());
+    const reiteratePrompt = Array.isArray(req.body.reiteratePrompt)
+        ? req.body.reiteratePrompt.map((msg) => escapeBraces(msg))
+        : [];
+    const reiterateMessages = reiteratePrompt.map((msg) => ["system", msg]);
+    // const model = new ChatGroq({
+    //     apiKey: process.env.GROQ_API_KEY,
+    //     modelName: "llama3-70b-8192",
+    //     maxTokens: 8000,
+    //     stop: [], // Prevent early truncation
+    //     temperature: 0.1
+    // });
+    const model = new mistralai_1.ChatMistralAI({
+        model: "mistral-large-latest",
+        temperature: 0.1,
+        maxTokens: 8000,
+    });
     const chatPrompt = prompts_1.ChatPromptTemplate.fromMessages([
-        ["system", systemPrompt],
+        ["system", "You MUST generate responses between 4000-8000 tokens. Make the componenets extremely beautiful" + systemPrompt],
         ["human", basePrompt],
         ["human", filePrompt],
         ["human", userPrompt],
+        ...reiterateMessages
     ]);
-    const chain = chatPrompt.pipe(mistralModel);
+    const chain = chatPrompt.pipe(model);
     const response = yield chain.invoke({});
     res.json({
         response: (response.content)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,33 +2,36 @@ require('dotenv').config();
 import express from "express";
 
 import { ChatMistralAI } from "@langchain/mistralai";
+import { ChatGroq } from "@langchain/groq";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { BASE_PROMPT, getSystemPrompt } from "./prompt";
 import { basePrompt as nodeBasePrompt } from "./defaults/node";
 import { basePrompt as reactBasePrompt } from "./defaults/react";
+import cors from "cors";
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 
 function escapeBraces(str: string) {
     return str.replace(/{/g, '{{').replace(/}/g, '}}');
 }
 
-const mistralModel = new ChatMistralAI({
-    model: "mistral-large-latest",
-    temperature: 0.1,
-    maxTokens: 8000,
-});
-
 app.post("/template", async (req, res) => {
     const userPrompt = req.body.prompt;
+
+    const model = new ChatMistralAI({
+        model: "mistral-large-latest",
+        temperature: 0.1,
+        maxTokens: 8000,
+    });
 
     const checkProjectType = ChatPromptTemplate.fromMessages([
         ["system", "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra"],
         ["human", userPrompt],
     ]);
 
-    const chain = checkProjectType.pipe(mistralModel);
+    const chain = checkProjectType.pipe(model);
 
     const response = await chain.invoke({});
 
@@ -37,17 +40,20 @@ app.post("/template", async (req, res) => {
     if (answer == "react") {
         res.json({
             // This prompt is for llm so more context
-            prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            basePrompt: BASE_PROMPT,
+            filePrompt: `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+
             // This one is for frontend so we can make all the files and struture and and everything, thats why passing same thing twice
-            uiPrompts: [reactBasePrompt]
+            uiPrompts: reactBasePrompt
         })
         return;
     }
 
     if (answer === "node") {
         res.json({
-            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
-            uiPrompts: [nodeBasePrompt]
+            basePrompt: BASE_PROMPT,
+            filePrompt: `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+            uiPrompts: nodeBasePrompt
         })
         return;
     }
@@ -58,19 +64,40 @@ app.post("/template", async (req, res) => {
 });
 
 app.post("/chat", async (req, res) => {
-    const basePrompt = escapeBraces(req.body.basePrompt);
-    const filePrompt = escapeBraces(req.body.filePrompt);
-    const userPrompt = escapeBraces(req.body.userPrompt);
+    const basePrompt = escapeBraces(req.body.basePrompt ?? "");
+    const filePrompt = escapeBraces(req.body.filePrompt ?? "");
+    const userPrompt = escapeBraces(req.body.userPrompt ?? "");
     const systemPrompt = escapeBraces(getSystemPrompt());
 
+    const reiteratePrompt = Array.isArray(req.body.reiteratePrompt) 
+    ? req.body.reiteratePrompt.map((msg:string )=> escapeBraces(msg)) 
+    : [];    
+
+    const reiterateMessages = reiteratePrompt.map((msg:string) => ["system", msg]);
+
+    // const model = new ChatGroq({
+    //     apiKey: process.env.GROQ_API_KEY,
+    //     modelName: "llama3-70b-8192",
+    //     maxTokens: 8000,
+    //     stop: [], // Prevent early truncation
+    //     temperature: 0.1
+    // });
+
+    const model = new ChatMistralAI({
+        model: "mistral-large-latest",
+        temperature: 0.1,
+        maxTokens: 8000,
+    });
+
     const chatPrompt = ChatPromptTemplate.fromMessages([
-        ["system", systemPrompt],
+        ["system", "You MUST generate responses between 4000-8000 tokens. Make the componenets extremely beautiful" + systemPrompt],
         ["human", basePrompt],
         ["human", filePrompt],
         ["human", userPrompt],
+        ...reiterateMessages
     ]);
 
-    const chain = chatPrompt.pipe(mistralModel);
+    const chain = chatPrompt.pipe(model);
 
     const response = await chain.invoke({});
 
